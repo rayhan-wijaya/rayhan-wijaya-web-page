e5b3f30ad2a1722787ee33d0ee0c527da56e219e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 16:10:45 +0700

    removed vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    adapter: vercel({
-        analytics: true,
-    }),
 });
